c9dc15913bf026ffa3eff9b4fbeabdad2c5827ba
Akshay <akshaytondase96@gmail.com>
2024-03-21 14:55:28 +0530

    23-03-24 Committed2;55pm

--- src/test/java/datadriven/AutomationFrameworkTesting/StandAloneTest.java
+++ src/test/java/datadriven/AutomationFrameworkTesting/StandAloneTest.java
@@ -21,7 +21,7 @@ import org.testng.annotations.Test;
 import TestComponents.BaseTest;
 import TestComponents.Retry;
 import io.github.bonigarcia.wdm.WebDriverManager;
-//21-03-24-fourth commit
+//21-03-24-fifth commit
 public class StandAloneTest extends BaseTest
 {
      @Test(dataProvider= "getData",groups= {"Purchase"})
